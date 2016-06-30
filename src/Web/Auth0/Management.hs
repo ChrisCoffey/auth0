f8a513f73b619ac5e5d438ef755cd3bf1ff27348
Chris Coffey <cmcof273@gmail.com>
2016-06-30 13:09:56 -0400

    Fix sill build break

--- src/Web/Auth0/Management.hs
+++ src/Web/Auth0/Management.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ConstraintKinds #-}
 
 module Web.Auth0.Management(
     module Web.Auth0.Types,
@@ -13,13 +14,12 @@ import Web.Auth0.Types
 import Web.Auth0.Common
 
 import Control.Lens (view)
-import Control.Monad.Except (MonadError)
-import Control.Monad.Reader (MonadReader)
-import Control.Monad.Trans (MonadIO)
 import Data.Aeson
 import qualified Data.ByteString.Char8 as B
 import Network.HTTP.Nano
 import Network.HTTP.Types.URI (urlEncode)
+
+type Auth0M m r e = (HttpM m r e, HasAuth0 r)
 
 -- |Search users based on a lucene query into user profile fields
 searchUsers :: (Auth0M m r e, FromJSON a, FromJSON b) => Query -> m [Profile' a b]
@@ -32,25 +32,25 @@ getUser :: (Auth0M m r e, FromJSON a, FromJSON b) => String -> m (Profile' a b)
 getUser uid = httpJSON =<< a0Req GET ("api/v2/users/"++uid) NoRequestData
 
 -- |Create an email user
-createEmailUser :: (MonadIO m, MonadError e m, MonadReader r m, AsHttpError e, HasHttpCfg r, HasAuth0 r, FromJSON a, FromJSON b) => NewEmailUser -> m (Profile' a b)
+createEmailUser :: (Auth0M m r e, HasAuth0 r, FromJSON a, FromJSON b) => NewEmailUser -> m (Profile' a b)
 createEmailUser dta = httpJSON =<< a0Req POST "api/v2/users" (mkJSONData dta)
 
 -- |Set the email address of a profile
 setEmail :: (Auth0M m r e, FromJSON a, FromJSON b) => String -> String -> m (Profile' a b)
 setEmail uid email = do
     let dta = mkJSONData $ object ["email" .= email, "verify_email" .= False, "email_verified" .= True]
-    httpJSON =<< a0Req PATCH ("api/v2/users/"++uid) dta
+    httpJSON =<< a0Req pATCH ("api/v2/users/"++uid) dta
 
 -- |Set the phone number of a profile
 setPhone :: (Auth0M m r e, FromJSON a, FromJSON b) => String -> String -> m (Profile' a b)
 setPhone uid phone = do
     let dta = mkJSONData $ object ["phone_number" .= phone, "verify_phone_number" .= False, "phone_verified" .= True]
-    httpJSON =<< a0Req PATCH ("api/v2/users/"++uid) dta
+    httpJSON =<< a0Req pATCH ("api/v2/users/"++uid) dta
 
-setAppMetadata :: (Auth0M m r e, ToJson d, FromJSON a, FromJSON b) => String -> d -> m (Profile' a b)
+setAppMetadata :: (Auth0M m r e, ToJSON d, FromJSON a, FromJSON b) => String -> d -> m (Profile' a b)
 setAppMetadata uid metaData = do
   let dta = mkJSONData $ object [ "app_metadata" .= toJSON metaData ] 
-  httpJSON =<< a0Req PATCH ("api/v2/users/"++uid) dta
+  httpJSON =<< a0Req pATCH ("api/v2/users/"++uid) dta
 
 --
 -- Utility
@@ -62,4 +62,4 @@ a0Req mthd path dta = do
     tok <- view auth0Token
     addHeaders [("Content-Type", "application/json"),("Authorization", "Bearer " ++ tok)] <$> buildReq mthd url dta
 
-PATCH = CustomMethod "PATCH"
+pATCH = CustomMethod "PATCH"
